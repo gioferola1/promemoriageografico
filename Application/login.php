20793c0182f535ddb57d006db213a967c983cf11
gioferola1 <83026973+gioferola1@users.noreply.github.com>
2021-04-29 15:21:59 +0200

    Update login.php

--- Application/login.php
+++ Application/login.php
@@ -1,3 +1,22 @@
 <?php
-echo "ciao";
+//$email = $_POST['email'];
+//$password = $_POST['password'];
+$host="datipomemoria.mysql.database.azure.com";
+$dbUser="gioferola@datipomemoria";
+$dbPwd="fufnusufre.90";
+$dbName="promemoria";
+$conn = new mysqli($host, $dbUser, $dbPwd, $dbName);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$password = sha1($password);
+$sql = "show tables";
+$result = $conn->query($sql);
+$tabelle = new array();
+
+if($result->num_rows > 0){
+    while ($tabella = $result->fetch_assoc()){
+        $tabelle[] = $tabella;
+} 
+print json_encode($ristoranti);
 ?>
