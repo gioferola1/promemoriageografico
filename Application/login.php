1c348eb30b09c9f4850f695c992e9acbec3bcccd
gioferola1 <83026973+gioferola1@users.noreply.github.com>
2021-04-29 16:05:22 +0200

    Update login.php

--- Application/login.php
+++ Application/login.php
@@ -7,13 +7,13 @@ $dbPwd="fufnusufre.90";
 $dbName="promemoria";
 $conn=mysqli_init(); 
 mysqli_ssl_set($conn, NULL, NULL, "/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
- mysqli_real_connect($conn, "datipomemoria.mysql.database.azure.com", "gioferola@datipomemoria", "fufnusudre.90", "promemoria", 3306);
+ mysqli_real_connect($conn, "datipomemoria.mysql.database.azure.com", "gioferola@datipomemoria", "fufnusufre.90", "promemoria", 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 printf("Reading data from table: \n");
-$res = mysqli_query($conn, 'SELECT * FROM Products');
+$res = mysqli_query($conn, 'SELECT * FROM persone');
 while ($row = mysqli_fetch_assoc($res))
  {
     var_dump($row);
