f61ea7c6240cb99ae3f22d1669692e7da38d8ee4
gioferola1 <83026973+gioferola1@users.noreply.github.com>
2021-04-30 08:26:43 +0200

    Update login.php

--- Application/login.php
+++ Application/login.php
@@ -6,8 +6,8 @@ $dbUser="gioferola@datipomemoria";
 $dbPwd="fufnusufre.90";
 $dbName="promemoria";
 $conn=mysqli_init(); 
-mysqli_ssl_set($conn, NULL, NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
- mysqli_real_connect($conn, "datipomemoria.mysql.database.azure.com", "gioferola@datipomemoria", "fufnusufre.90", "promemoria", 3306);
+mysqli_ssl_set($conn, NULL, NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+ mysqli_real_connect($conn, "datipomemoria.mysql.database.azure.com", "gioferola@datipomemoria", "fufnusufre.90", "promemoria", 3306,MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
